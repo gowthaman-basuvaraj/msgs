b85ea396d099b346d8d409ce57cc8a7d450962f1
gowthaman <gowthaman@365build.in>
2020-11-13 08:12:27 +0530

    handle multi-part sms

--- app/src/main/java/com/webianks/hatkemessenger/receivers/SmsReceiver.kt
+++ app/src/main/java/com/webianks/hatkemessenger/receivers/SmsReceiver.kt
@@ -32,119 +32,126 @@ class SmsReceiver : BroadcastReceiver() {
             Log.e(TAG, "smsReceiver")
             val bundle = intent.extras ?: return
             val pdu_Objects = bundle["pdus"] as Array<Any>? ?: return
+
+            var senderNoOriginal = ""
+            var message = ""
+            var ts = 0L
             for (aObject in pdu_Objects) {
                 val currentSMS = getIncomingMessage(aObject, bundle)
-                val senderNoOriginal = currentSMS.displayOriginatingAddress
-                val message = currentSMS.displayMessageBody
-                //for INDIA, we get lot of SMS from Banks and other providers of this format
-                //2 Alphabets - (hyhen) 6 Alphabets
-                //first 2 will change but the last 6 will remain same,
-                //lets group by last 6 if it matched the pattern
+                senderNoOriginal = currentSMS.displayOriginatingAddress
+                message += currentSMS.displayMessageBody
+                ts = currentSMS.timestampMillis
+            }
 
 
-                val lookupPerson = PersonLookup(context).lookupPerson(senderNoOriginal)
-                val cn = lookupPerson?.name ?: lookupPerson?.normPhone ?: senderNoOriginal
-                val senderNo = lookupPerson?.normPhone ?: senderNoOriginal
-                val existingNC = getChannel(senderNo, context)
-                if (existingNC != null) {
-                    if (existingNC.importance != NotificationManager.IMPORTANCE_NONE) {
-                        issueNotification(context, senderNo, message, cn)
-                    } else {
-                        //do not issue
-                    }
+            //for INDIA, we get lot of SMS from Banks and other providers of this format
+            //2 Alphabets - (hyhen) 6 Alphabets
+            //first 2 will change but the last 6 will remain same,
+            //lets group by last 6 if it matched the pattern
+
+
+            val lookupPerson = PersonLookup(context).lookupPerson(senderNoOriginal)
+            val cn = lookupPerson?.name ?: lookupPerson?.normPhone ?: senderNoOriginal
+            val senderNo = lookupPerson?.normPhone ?: senderNoOriginal
+            val existingNC = getChannel(senderNo, context)
+            if (existingNC != null) {
+                if (existingNC.importance != NotificationManager.IMPORTANCE_NONE) {
+                    issueNotification(context, senderNo, message, cn)
                 } else {
-                    //create a channal but set it to no notify...
-                    createChannel(cn, "SMS Notifications", context)
-                    //we will not notify by default :P
-                    //issueNotification(context, senderNo, message, cn)
+                    //do not issue
                 }
-                saveSmsInInbox(context,
-                        currentSMS.displayOriginatingAddress,
-                        currentSMS.displayMessageBody,
-                        currentSMS.timestampMillis)
+            } else {
+                //create a channal but set it to no notify...
+                createChannel(cn, "SMS Notifications", context)
+                //we will not notify by default :P
+                //issueNotification(context, senderNo, message, cn)
             }
-            abortBroadcast()
-            // End of loop
-        } // bundle null
-
-    }
-
-    private fun saveSmsInInbox(context: Context, sender: String, mesg: String, date: Long) {
-        val serviceIntent = Intent(context, SaveSmsService::class.java)
-        serviceIntent.putExtra("sender_no", sender)
-        serviceIntent.putExtra("message", mesg)
-        serviceIntent.putExtra("date", date)
-        context.startService(serviceIntent)
-    }
-
-    private fun issueNotification(context: Context, senderNo: String, message: String, cn: String) {
-        val resultIntent = Intent(context, SmsDetailedView::class.java)
-        resultIntent.putExtra(Constants.CONTACT_NAME, senderNo)
-        resultIntent.putExtra(Constants.FROM_SMS_RECIEVER, true)
-        val resultPendingIntent = PendingIntent.getActivity(
-                context,
-                0,
-                resultIntent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-        )
-
-        val icon = BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launcher)
-        val mNotifyMgr = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-
-        val mBuilder = NotificationCompat.Builder(context, cn)
-                .setLargeIcon(icon)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle(cn)
-                .setStyle(NotificationCompat.BigTextStyle().bigText(message))
-                .setAutoCancel(true)
-                .setContentText(message)
-                .setContentIntent(resultPendingIntent)
-                .setStyle(NotificationCompat.BigTextStyle().bigText(message))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        val mNotificationId = 10001
-        mNotifyMgr.notify(mNotificationId, mBuilder.build())
-
-    }
-
-    private fun createChannel(senderNo: String, message: String, context: Context) {
-        val mNotifyMgr = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(senderNo, senderNo, NotificationManager.IMPORTANCE_DEFAULT).apply {
-                description = message
-                importance = getImportanceLevel(senderNo)
-            }
-            mNotifyMgr.createNotificationChannel(channel)
+            saveSmsInInbox(context,
+                    senderNo,
+                    message,
+                    ts)
         }
-    }
+        abortBroadcast()
+        // End of loop
+    } // bundle null
 
-    @SuppressLint("InlinedApi")
-    private fun getImportanceLevel(senderNo: String): Int {
-        //by default we will not notify for anything
-        //however we will notify based on preference
-        return NotificationManager.IMPORTANCE_NONE
-    }
+}
 
-    private fun getChannel(senderNo: String, context: Context): NotificationChannel? {
-        val mNotifyMgr = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+private fun saveSmsInInbox(context: Context, sender: String, mesg: String, date: Long) {
+    val serviceIntent = Intent(context, SaveSmsService::class.java)
+    serviceIntent.putExtra("sender_no", sender)
+    serviceIntent.putExtra("message", mesg)
+    serviceIntent.putExtra("date", date)
+    context.startService(serviceIntent)
+}
 
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            mNotifyMgr.getNotificationChannel(senderNo)
-        } else {
-            null
+private fun issueNotification(context: Context, senderNo: String, message: String, cn: String) {
+    val resultIntent = Intent(context, SmsDetailedView::class.java)
+    resultIntent.putExtra(Constants.CONTACT_NAME, senderNo)
+    resultIntent.putExtra(Constants.FROM_SMS_RECIEVER, true)
+    val resultPendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            resultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    val icon = BitmapFactory.decodeResource(context.resources, R.mipmap.ic_launcher)
+    val mNotifyMgr = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+    val mBuilder = NotificationCompat.Builder(context, cn)
+            .setLargeIcon(icon)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle(cn)
+            .setStyle(NotificationCompat.BigTextStyle().bigText(message))
+            .setAutoCancel(true)
+            .setContentText(message)
+            .setContentIntent(resultPendingIntent)
+            .setStyle(NotificationCompat.BigTextStyle().bigText(message))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+    val mNotificationId = 10001
+    mNotifyMgr.notify(mNotificationId, mBuilder.build())
+
+}
+
+private fun createChannel(senderNo: String, message: String, context: Context) {
+    val mNotifyMgr = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val channel = NotificationChannel(senderNo, senderNo, NotificationManager.IMPORTANCE_DEFAULT).apply {
+            description = message
+            importance = getImportanceLevel(senderNo)
         }
+        mNotifyMgr.createNotificationChannel(channel)
     }
+}
 
-    private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
-        val currentSMS: SmsMessage
-        currentSMS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val format = bundle.getString("format")
-            SmsMessage.createFromPdu(aObject as ByteArray, format)
-        } else {
-            SmsMessage.createFromPdu(aObject as ByteArray)
-        }
-        return currentSMS
+@SuppressLint("InlinedApi")
+private fun getImportanceLevel(senderNo: String): Int {
+    //by default we will not notify for anything
+    //however we will notify based on preference
+    return NotificationManager.IMPORTANCE_NONE
+}
+
+private fun getChannel(senderNo: String, context: Context): NotificationChannel? {
+    val mNotifyMgr = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        mNotifyMgr.getNotificationChannel(senderNo)
+    } else {
+        null
     }
+}
+
+private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
+    val currentSMS: SmsMessage
+    currentSMS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val format = bundle.getString("format")
+        SmsMessage.createFromPdu(aObject as ByteArray, format)
+    } else {
+        SmsMessage.createFromPdu(aObject as ByteArray)
+    }
+    return currentSMS
 }
