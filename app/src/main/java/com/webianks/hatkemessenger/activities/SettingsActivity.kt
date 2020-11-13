be386f7ab2b5fd0be7bea3dc84d6e1d299067714
gowthaman <gowthaman@365build.in>
2020-11-13 08:22:13 +0530

    cleanup preferences

--- app/src/main/java/com/webianks/hatkemessenger/activities/SettingsActivity.kt
+++ app/src/main/java/com/webianks/hatkemessenger/activities/SettingsActivity.kt
@@ -1,27 +1,20 @@
 package com.webianks.hatkemessenger.activities
 
-import android.app.ProgressDialog
 import android.os.Bundle
-import android.preference.Preference
-import android.preference.Preference.OnPreferenceClickListener
-import android.preference.PreferenceFragment
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
 import com.webianks.hatkemessenger.R
-import com.webianks.hatkemessenger.constants.Constants
 
 class SettingsActivity : AppCompatActivity() {
-
-    private var progressDialog: ProgressDialog? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_layout)
         if (supportActionBar != null) supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-        fragmentManager.beginTransaction().replace(R.id.container,
-                MyPreferenceFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.container,
+                SettingsFragment()).commit()
 
     }
 
@@ -30,24 +23,10 @@ class SettingsActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    class MyPreferenceFragment : PreferenceFragment() {
-        private var drivePref: Preference? = null
-        private var restorePref: Preference? = null
-        private val TAG = MyPreferenceFragment::class.java.simpleName
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            addPreferencesFromResource(R.xml.pref_general)
-            drivePref = findPreference(Constants.BACKUP)
-            drivePref?.onPreferenceClickListener = OnPreferenceClickListener {
-                true
-            }
-            restorePref = findPreference(Constants.RESTORE)
-            restorePref?.onPreferenceClickListener = OnPreferenceClickListener { //backup dialog
-                true
-            }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.pref_general, rootKey)
         }
-
     }
-
-
 }
